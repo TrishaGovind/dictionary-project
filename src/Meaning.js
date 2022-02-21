1ac9cc5edb548022b2aa90a79b6a81a1432cebfb
TrishaGovind <trisha.govind@gmail.com>
2022-02-21 18:35:06 +0800

    Improved phonetic audio playback

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,10 +9,9 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <div className="definition">{definition.definition}</div>
-
             <div className="example">{definition.example}</div>
-            <br />
             <Synonyms synonyms={definition.synonyms} />
+            <br />
           </div>
         );
       })}
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,21 +1,27 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <div className="row">
-        <div className="col-1">
-          <a
-            href={props.phonetic.audio}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            &#9654;
-          </a>
+  let audio = new Audio(props.phonetic.audio);
+  const start = () => {
+    audio.play();
+  };
+
+  if (props.phonetic.audio) {
+    return (
+      <div className="Phonetic">
+        <div className="row mb-2">
+          <div className="col-1">
+            <a href={Audio} onClick={start}>
+              <FontAwesomeIcon icon={faCirclePlay} className="button" />
+            </a>
+          </div>
+          <div className="col pt-1 ps-3 text">{props.phonetic.text}</div>
         </div>
-        <div className="col pt-3 ps-3 text">{props.phonetic.text}</div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
