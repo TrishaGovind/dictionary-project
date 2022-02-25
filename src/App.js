44fbc8b5d8517d93186dcd12509ad78d545051f8
TrishaGovind <trisha.govind@gmail.com>
2022-02-25 15:44:42 +0800

    Improved mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import "./fonts/BlackMango-Medium.ttf";
 import logo from "./logo.png";
+import tg from "./tg-icon.png";
+import github from "./github-icon.png";
+import netlify from "./netlify-icon.png";
+import react from "./react-icon.png";
 import Dictionary from "./Dictionary.js";
 
 function App() {
@@ -19,23 +23,57 @@ function App() {
         </header>
         <Dictionary defaultKeyword="dictionary" />
 
-        <footer className="text-center mt-4">
-          This project was coded by{" "}
-          <a
-            href="https://trisha-govind.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Trisha Govind
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/TrishaGovind/dictionary-project"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
+        <footer className="text-center mt-2 mb-4">
+          <div className="row">
+            <div className="col-3 mb-2">
+              <img
+                src={react}
+                className="react-logo img-fluid"
+                alt="react icon"
+                width={30}
+              />
+            </div>
+            <div className="col-3 mb-2">
+              <a
+                href="https://trisha-govind.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={tg}
+                  className="tg-logo img-fluid mx-auto"
+                  alt="website"
+                  width={30}
+                />
+              </a>
+            </div>
+            <div className="col-3 mb-2">
+              <a
+                href="https://github.com/TrishaGovind/dictionary-project"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={github}
+                  className="github-logo img-fluid"
+                  alt="github icon"
+                  width={30}
+                />
+              </a>
+            </div>
+            <div className="col-3 mb-2">
+              <img
+                src={netlify}
+                className="netlify-logo img-fluid"
+                alt="netlify icon"
+                width={30}
+              />
+            </div>
+            <div className="col-3">Built with React</div>
+            <div className="col-3">Coded by Trisha Govind</div>
+            <div className="col-3">Open-sourced on GitHub</div>
+            <div className="col-3">Hosted on Netlify</div>
+          </div>
         </footer>
       </div>
     </div>
--- src/Photos.js
+++ src/Photos.js
@@ -1,28 +1,58 @@
 import React from "react";
+import { Carousel } from "react-bootstrap";
+import Media from "react-media";
 
 export default function Photos(props) {
   if (props.photos) {
     return (
-      <section className="Photos">
-        <div className="row">
-          {props.photos.map(function (photo, index) {
-            return (
-              <div className="col-4" key={index}>
-                <a
-                  href={photo.src.original}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src={photo.src.landscape}
-                    className="img-fluid"
-                    alt={photo.alt}
-                  />
-                </a>
+      <section className="Photos pb-1">
+        <Media query="(max-width: 600px)">
+          {(matches) =>
+            matches ? (
+              <Carousel>
+                {props.photos.map(function (photo, index) {
+                  return (
+                    <Carousel.Item>
+                      <div key={index}>
+                        <a
+                          href={photo.src.original}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img
+                            src={photo.src.landscape}
+                            className="img-fluid"
+                            alt={photo.alt}
+                          />
+                        </a>
+                      </div>
+                    </Carousel.Item>
+                  );
+                })}
+              </Carousel>
+            ) : (
+              <div className="row">
+                {props.photos.map(function (photo, index) {
+                  return (
+                    <div className="col-4" key={index}>
+                      <a
+                        href={photo.src.original}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img
+                          src={photo.src.landscape}
+                          className="img-fluid"
+                          alt={photo.alt}
+                        />
+                      </a>
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
+            )
+          }
+        </Media>
       </section>
     );
   } else {
